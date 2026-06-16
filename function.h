670b9301045a8ad03ee0a7ec8fe469b38d8724fc
shinhiy58@gmail.com <2601110281@gspace.kopo.ac.kr>
2026-06-16 20:43:25 +0900

    초기 커밋.

---
+++ function.h
@@ -0,0 +1,17 @@
+﻿#ifndef FUNCTION_H
+#define FUNCTION_H
+
+#include <windows.h>
+
+// 메인에 있는 mapnum 변수를 공유하겠다고 선언
+extern int mapnum;
+
+extern int see;
+
+extern int gameset;
+// 다른 파일에서 쓸 함수들의 모양 선언
+void map(int num);
+DWORD WINAPI monstermove(LPVOID lpParam);
+
+DWORD WINAPI seenum(LPVOID lpParam);
+#endif
---
+++ tr.c
@@ -0,0 +1,57 @@
+﻿#include "function.h"
+#include <stdlib.h>
+#include <conio.h>
+#include <windows.h>
+
+// 진짜 변수 생성 및 초기화
+int mapnum = 0;
+int see = 0;
+int gameset = 0;
+
+
+int main() {
+	HANDLE MonsterThread = CreateThread(NULL, 0, monstermove, NULL, 0, NULL);
+	HANDLE SeeThread = CreateThread(NULL, 0, seenum, NULL, 0, NULL);
+
+	if (MonsterThread == NULL || SeeThread == NULL) {
+
+		// 생성에 성공한 스레드가 있다면 그것만 닫아줍니다.
+		if (MonsterThread != NULL) CloseHandle(MonsterThread);
+		if (SeeThread != NULL) CloseHandle(SeeThread);
+
+		return 1; // 프로그램 종료
+	};
+
+	map(mapnum);
+	while (1) {
+		if (gameset == 0) {
+			Sleep(500);
+			if (see == 119) {
+				if (mapnum == 0) {
+					system("cls");
+					map(0);
+				}
+				else {
+					system("cls");
+					map(mapnum);
+				}
+			}
+			else if (see == 97) {
+				system("cls");
+				map(1);
+			}
+			else if (see == 100) {
+				system("cls");
+				map(2);
+			}
+		}
+		else if (gameset == 1) {
+			system("cls");
+			break;
+		}
+	}
+	WaitForSingleObject(MonsterThread, INFINITE);
+	WaitForSingleObject(SeeThread, INFINITE);
+	CloseHandle(MonsterThread);
+	CloseHandle(SeeThread);
+}
